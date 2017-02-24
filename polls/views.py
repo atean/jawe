ef474b9eeec66c19c8516b4e9e3015fd9c69fb1e
atean <rr.dhanokar@gmail.com>
2017-02-24 19:38:14 +0530

    added templates

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 # Create your views here.
 
 def poll_index(request):
-   return HttpResponse("Now you will see me")
+    template = loader('polls/index.html',)
+
+def detail(request, question_id):
+    pass
+
+def vote(request, question_id):
+    pass
