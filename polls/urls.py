a56c474c2dc7f14cd846b73726f5ce9b0ae3127b
atean <rr.dhanokar@gmail.com>
2017-02-27 11:15:31 +0530

    added template rendering

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
+
+# noinspection PyInterpreter
 urlpatterns = [
-    url(r'^$', views.poll_index, name='pindex')
+    url(r'^$', views.index, name='index', ),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
-# Create your views here.
 
-def poll_index(request):
-    template = loader('polls/index.html',)
+# Create your views here.
+from models import Question, Choice
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-published_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 def detail(request, question_id):
-    pass
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def vote(request, question_id):
     pass
